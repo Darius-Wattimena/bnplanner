bad35faddcd406e152143a04737a89b85def53c2
Darius Wattimena <dariuswattimena@hotmail.com>
2020-07-15 22:31:26 +0200

    Fixed an issue where the nominator didn't get selected properly

--- src/main/kotlin/nl/greaper/bnplanner/dataSource/BeatmapDataSource.kt
+++ src/main/kotlin/nl/greaper/bnplanner/dataSource/BeatmapDataSource.kt
@@ -49,7 +49,7 @@ class BeatmapDataSource(database: MongoDatabase) {
                         if (filter.artist != null) { Beatmap::artist regex quote(filter.artist).toRegex(RegexOption.IGNORE_CASE) } else null,
                         if (filter.title != null) { Beatmap::title regex quote(filter.title).toRegex(RegexOption.IGNORE_CASE) } else null,
                         if (filter.mapper != null) { Beatmap::mapper regex quote(filter.mapper).toRegex(RegexOption.IGNORE_CASE) } else null,
-                        if (filter.nominator.isNotEmpty()) { Beatmap::nominators `in` filter.nominator } else null,
+                        if (filter.nominator.any { it != 0L }) { Beatmap::nominators `in` filter.nominator.filter { it != 0L } } else null,
                         if (filter.hideRanked != null && !filter.hideRanked) null else Beatmap::status ne BeatmapStatus.Ranked.prio,
                         if (filter.hideGraved != null && !filter.hideGraved) null else Beatmap::status ne BeatmapStatus.Graved.prio,
                         if (filter.hideWithTwoNominators != null && filter.hideWithTwoNominators) { Beatmap::nominators `in` listOf<Long>(0) } else null
