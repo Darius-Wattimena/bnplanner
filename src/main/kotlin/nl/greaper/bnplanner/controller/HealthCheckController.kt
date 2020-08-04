e12343f3b68c0b05d57cc50f897b814941fc1322
Darius Wattimena <dariuswattimena@hotmail.com>
2020-08-04 20:45:12 +0200

    Moved the healthcheck to the root to make it easier for the backend to setup in aws

--- src/main/kotlin/nl/greaper/bnplanner/controller/HealthCheckController.kt
+++ src/main/kotlin/nl/greaper/bnplanner/controller/HealthCheckController.kt
@@ -2,16 +2,14 @@ package nl.greaper.bnplanner.controller
 
 import nl.greaper.bnplanner.model.HealthCheck
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/v1")
 class HealthCheckController {
 
-    @GetMapping("/healthcheck")
+    @GetMapping("/")
     fun healthCheck(): HealthCheck {
-        return HealthCheck("100")
+        return HealthCheck("200")
     }
 
 }
